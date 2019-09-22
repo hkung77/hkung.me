a33af595ff444e3be2f46bd2b58a93ec494fa22e
hkung77 <henrykung@live.ca>
2019-09-23 00:03:33 +0900

    include several css libraries

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,18 @@
+// Adds black background to navbar when user starts scrolling
 const collapseNavBar = () => {
     const mainNav = document.getElementById('main-nav');
-    if (mainNav) {
-        console.log(mainNav);
+    if (window.pageYOffset > 0) {
+        mainNav.classList.remove("bg-transparent");
+        mainNav.classList.add("bg-black");
+    } else if (window.pageYOffset === 0) {
+        mainNav.classList.remove("bg-black");
+        mainNav.classList.add("bg-transparent");
     }
 }
 
 
+
+// Event handler when window scrolls
+window.onscroll = () => {
+    collapseNavBar();
+};
